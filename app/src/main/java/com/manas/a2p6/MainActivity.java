82b9688d058102120e1c0023f7faae4597f90924
manastyagi <manasvi.tyagi@gmail.com>
2020-10-11 07:17:10 +1300

    Assignment 2 part6.1,6.2,6.3

---
+++ app/src/main/java/com/manas/a2p6/MainActivity.java
@@ -0,0 +1,202 @@
+package com.manas.a2p6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    public GoogleMap mMap;
+    Button locateBtn;
+    Button weatherBtn;
+    TextView locationField;
+    TextView tempratureField;
+    public double latitude = 0.00;
+    public double longitude = 0.00;
+    private static final int REQUEST_CODE_LOC_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        locateBtn = (Button) findViewById(R.id.locateButton);
+        weatherBtn = (Button) findViewById(R.id.getTempratureButton);
+        locationField = findViewById(R.id.location_field);
+        tempratureField = findViewById(R.id.currentTemprature_field);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapsFragment()).commit();
+
+        locateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Also sets the location globally
+                //subsequently sets tge location on the map in fragment
+                getpermission();
+            }
+        });
+        
+        weatherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateWeather();
+            }
+        });
+
+
+    }
+
+    private void updateWeather() {
+        //Since this is network request
+        //We need to put it on an async queue to be processed
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = getUrl();
+
+        JsonObjectRequest jsonObjectRequest = new
+                JsonObjectRequest(Request.Method.GET, url,
+                new JSONObject(),
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String server_data = response.toString();
+                            JSONObject json_outer_obj = new JSONObject(server_data);
+                            JSONObject currentValue = json_outer_obj.getJSONObject("current");
+                            JSONObject json_inner_current_obj = new JSONObject(currentValue.toString());
+                            String currentTemp = json_inner_current_obj.getString("temp");
+                            String currentPressure = json_inner_current_obj.getString("pressure");
+                            String currentWindSpeed = json_inner_current_obj.getString("wind_speed");
+                            String humidity = json_inner_current_obj.getString("humidity");
+                            String windDirection = json_inner_current_obj.getString("wind_deg");
+                            Double tempInCelcius = Double.valueOf(currentTemp) - 273.15;
+
+                            String result = "Temperature: " + tempInCelcius + "\n" +
+                                    "Pressure: " + currentPressure + "\n" +
+                                    "Humidity: " + humidity + "\n" +
+                                    "Wind Direction: " + windDirection + "\n" +
+                                    "Wind Speed: " + currentWindSpeed;
+
+                            tempratureField.setText(String.valueOf(tempInCelcius));
+                            Log.e("data", result);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", error.toString());
+            }
+        });
+        queue.add(jsonObjectRequest);
+
+    }
+
+    //Gets the url to be called based on the current location
+    private String getUrl() {
+        return  "https://api.openweathermap.org/data/2.5/onecall?lat=LATTT&lon=LONGGG&appid=ab57f83f7f35c8c4355d9fb49b2d0f96"
+                .replace("LATTT",String.valueOf(this.latitude))
+                .replace("LONGGG",String.valueOf(this.longitude));
+    }
+
+    private void getpermission() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOC_PERMISSION);
+        } else {
+            getCurrentLocation();
+        }
+    }
+
+    private void getCurrentLocation() {
+        {
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(10000);
+            locationRequest.setFastestInterval(3000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                    .requestLocationUpdates(locationRequest, new LocationCallback() {
+
+                        @Override
+                        public void onLocationResult(LocationResult locationResult) {
+                            super.onLocationResult(locationResult);
+                            LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                                    .removeLocationUpdates(this);
+                            if (locationResult != null && locationResult.getLocations().size() > 0) {
+                                int latestLocationIndex = locationResult.getLocations().size() - 1;
+                                latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                                longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                                setLocationGlobally(latitude,longitude);
+                                // Add a marker in present cordinates and move the camera
+//                            LatLng presentCordinates = new LatLng(latitude, longitude);
+//                            mMap.addMarker(new MarkerOptions().position(presentCordinates).title("Marker in Present location"));
+//                            mMap.moveCamera(CameraUpdateFactory.newLatLng(presentCordinates));
+                                broadcastMessageToMapFragment(latitude, longitude);
+                                setLocationOnActivity(latitude, longitude);
+                            }
+                        }
+                    }, Looper.getMainLooper());
+        }
+    }
+
+    private void setLocationGlobally(double latitude, double longitude) {
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+    private void setLocationOnActivity(double latitude, double longitude) {
+        locationField.setText(String.valueOf(latitude)+ ", "+ longitude);
+    }
+
+    private void broadcastMessageToMapFragment(double latitude, double longitude) {
+        Intent intent = new Intent("STRING_ID_FOR_BRODCAST");
+        intent.putExtra("latitude", String.valueOf(latitude));
+        intent.putExtra("longitude", String.valueOf(longitude));
+        sendBroadcast(intent);
+    }
+}
---
+++ app/src/main/java/com/manas/a2p6/MapsFragment.java
@@ -0,0 +1,78 @@
+package com.manas.a2p6;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment {
+    public GoogleMap gMap;
+    public OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng sydney = new LatLng(-34, 151);
+            gMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        IntentFilter filter = new IntentFilter("STRING_ID_FOR_BRODCAST");
+        getActivity().registerReceiver(receiverUpdateDownload, filter);
+
+
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+    BroadcastReceiver receiverUpdateDownload = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            Double latitude = Double.valueOf(intent.getStringExtra("latitude"));
+            Double longitude = Double.valueOf(intent.getStringExtra("longitude"));
+            Log.e("data","current locations: "+ latitude+", "+longitude);
+            LatLng sydney = new LatLng(latitude, longitude);
+            gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    };
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
